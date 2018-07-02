4da49f1ec446a4dcab53a3485f4b990a738ba848
Abraham Chan <abchan@e1z3r3p6.42.us.org>
2018-07-01 19:16:16 -0700

    Edited styles for css and added img folder

--- front-end/my-app/src/components/Header/Header.js
+++ front-end/my-app/src/components/Header/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import cart from '../../img/shopping_cart.png'
+import profile from '../../img/profile.png'
+import admincrown from '../../img/admincrown.png'
 import './styles.css'
 
 class Header extends Component {
@@ -15,38 +17,38 @@ class Header extends Component {
 			<div>
 				<nav>
 					<ul>
-						<li><Link to='/'>Home</Link></li>
-						<li><Link to='/catalog'>Shop</Link></li>
-						<li><Link to='/login'>Login</Link></li>
-						<li><Link to='/cart'>Cart</Link></li>
-						<li><Link to='/profile'>Profile</Link></li>
+						{/* <li><Link to='/'>Home</Link></li> */}
+						<div className="shopbox"><li><Link to='/catalog' className="shoptext">Shop</Link></li></div>
+						<div className="loginbox"><li><Link to='/login' className="logintext">Login</Link></li></div>
+						<li><Link to='/cart'><img src={cart} className="cart" alt="shopping cart" title="shopping cart"/></Link></li>
+						<li><Link to='/profile'><img src={profile} className="profile" alt="my profile" title="my profile"/></Link></li>
 					</ul>
 				</nav>
 			</div>
-		);
+		)
 
 		this.loggedInAdmin = (
 			<div>
 				<nav>
 					<ul>
-						<li><Link to='/'>Home</Link></li>
-						<li><Link to='/catalog'>Shop</Link></li>
-						<li><Link to='/login'>Login</Link></li>
-						<li><Link to='/admin'>Admin</Link></li>
-						<li><Link to='/cart'>Cart</Link></li>
-						<li><Link to='/profile'>Profile</Link></li>
+						{/* <li><Link to='/'>Home</Link></li> */}
+						<div className="shopbox"><li><Link to='/catalog' className="shoptext">Shop</Link></li></div>
+						<div className="loginbox"><li><Link to='/login' className="logintext">Login</Link></li></div>
+						<li><Link to='/admin'><img src={admincrown} className="admincrown" alt="admin" title="admin"/></Link></li>
+						<li><Link to='/cart'><img src={cart} className="cart" alt="shopping cart" title="shopping cart"/></Link></li>
+						<li><Link to='/profile'><img src={profile} className="profile" alt="my profile" title="my profile"/></Link></li>
 					</ul>
 				</nav>
 			</div>
-		);
+		)
 
 		this.loggedOutMenu = (
 			<div>
 				<nav>
 					<ul>
-						<li><Link to='/'>Home</Link></li>
-						<li><Link to='/catalog'>Shop</Link></li>
-						<li><Link to='/login'>Login</Link></li>
+						{/* <li><Link to='/'>Home</Link></li> */}
+						<div className="shopbox"><li><Link to='/catalog' className="shoptext">Shop</Link></li></div>
+						<div className="loginbox"><li><Link to='/login' className="logintext">Login</Link></li></div>
 					</ul>
 				</nav>
 			</div>
@@ -67,8 +69,9 @@ class Header extends Component {
 	render() {
 		return (
 			<header>
-				<div className="header">Header</div>
+				<div className="header"><Link to='/' className="headertext">cool store name</Link></div>
 				{ this.state.nav }
+				<hr/>
 			</header>
 		)
 	}
