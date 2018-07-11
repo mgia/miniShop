b804a4e304834d9727685375e1236c530f814259
mtan <findmarcg@gmail.com>
2018-07-11 15:17:59 -0700

    Converted main render to const function

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App/App';
 
 import './index.css';
 
-render((
+const render = () => ReactDOM.render((
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>
 ), document.getElementById('root'));
+
+render()
